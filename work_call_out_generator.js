1e5d4040dd31e1f2b5748040508d8bd4f581d174
DGLewis <69864186+Super-Derek@users.noreply.github.com>
2024-02-07 00:15:26 -0500

    Build generateBody function framework

--- work_call_out_generator.js
+++ work_call_out_generator.js
@@ -69,6 +69,74 @@ const generateGreeting = (bossName = 'Mr(s). Boss', messageTone = 'respectful') 
     return greeting;
 };
 
+//Function to generate a random body message based on message tone preference
+//Input desired tone (professional || unprofessional || weird)
+const generateBody = (messageTone = 'professional') => {
+    const messageChoice = Math.floor(Math.random() * 5); //generates random number from 0-4
+    if (messageTone === 'professional') {
+        switch (messageChoice) {
+            case 0: 
+                body = `probody1`;
+                break;
+            case 1:
+                body = `probody2`;
+                break;
+            case 2:
+                body = `probody3`;
+                break;
+            case 3:
+                body = `probody4`;
+                break;
+            case 4:
+                body = `probody5`;
+                break;
+            default:
+                body = 'default';
+        } 
+    } else if (messageTone === 'unprofessional') {
+        switch (messageChoice) {
+            case 0: 
+                body = `unprobody1`;
+                break;
+            case 1:
+                body = `unprobody2`;
+                break;
+            case 2:
+                body = `unprobody3`;
+                break;
+            case 3:
+                body = `unprobody4`;
+                break;
+            case 4:
+                body = `unprobody5`;
+                break;
+            default:
+                body = 'default';
+        } 
+    } else if (messageTone === 'weird'){
+        switch (messageChoice) {
+            case 0: 
+                body = `weirdbody1`;
+                break;
+            case 1:
+                body = `weirdbody2`;
+                break;
+            case 2:
+                body = `weirdbody3`;
+                break;
+            case 3:
+                body = `weirdbody4`;
+                break;
+            case 4:
+                body = `weirdbody5`;
+                break;
+            default:
+                body = 'default';
+        }
+    }
+    return body;
+};
+
 
 
 
@@ -82,3 +150,7 @@ console.log(generateGreeting('Mr. Guy', 'disrespectful')); //expects Mr. Guy and
 console.log(generateGreeting()); //expects Mr(s). Boss and random respectful greeting
 console.log(generateGreeting('Mrs. Smith')); //expects Mrs. Smith and random respectful greeting
 console.log(generateGreeting('Capt. Blub', 'cringe')); //expects Capt. Bulb and random cringe greeting
+console.log(generateBody()); //expects rand pro body
+console.log(generateBody('professional')); //expects random pro body
+console.log(generateBody('unprofessional')); //expects rand unpro body
+console.log(generateBody('weird')); //expects rand weird body
